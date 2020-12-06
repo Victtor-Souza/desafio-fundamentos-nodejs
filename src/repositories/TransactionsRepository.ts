4cbc90132b5a0d23f838ad0b34b754865e9cfd85
Victtor De Souza Rodrigues <victtorsr96@gmail.com>
2020-12-05 23:30:31 -0300

    First Commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let outcome = 0;
+
+    const income =
+      this.transactions.length > 0
+        ? this.transactions
+            .filter(x => x.type === 'income')
+            .map(x => x.value)
+            .reduce((a, b) => a + b)
+        : 0;
+
+    const outcomeTransactions = this.transactions.filter(
+      x => x.type === 'outcome',
+    );
+
+    if (outcomeTransactions.length > 0) {
+      outcome = outcomeTransactions.map(x => x.value).reduce((a, b) => a + b);
+    }
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
